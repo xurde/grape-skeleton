a303f55da494f2240b2a6db2a0556c8c8dfc84a1
Jorge Díaz <jorgedf@gmail.com>
2015-06-15 22:03:58 +0200

    Rename logfile.

--- init.rb
+++ init.rb
@@ -2,6 +2,8 @@ def load_path(path)
   File.join(File.dirname(__FILE__), path)
 end
 
+RACK_ENV = ENV['RACK_ENV'] || 'development'
+
 $LOAD_PATH << load_path(".")
 $LOAD_PATH << load_path("./lib")
 
@@ -11,9 +13,7 @@ require 'middleware/logger'
 require 'logger'
 
 class ::Logger; alias_method :write, :<<; end # for Rack::CommonLogger
-$logger = ::Logger.new('log/service.log')
-
-RACK_ENV = ENV['RACK_ENV'] || 'development'
+$logger = ::Logger.new("log/#{RACK_ENV}.log")
 
 puts "Starting environment: #{RACK_ENV}..."
 @config = YAML.load_file('config/database.yml')[RACK_ENV]
